53080e57fbee4b31a1f3152275e6c6ddd06521f5
Mr_Choco <chocoretostonus@gmail.com>
2023-11-05 17:53:46 -0600

    Refactoration

---
+++ DC_Menu.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_Selector
+{
+    class DC_Menu
+    {
+
+        #region Public_Methods
+
+        public void Dot_Menu(string[] Options, Func<bool>[] Funcs, ConsoleColor[] Colors)
+        {
+            MainLoop(Options, Funcs, Colors);
+        }
+
+
+        public void Dot_Menu(string[] Options, Func<bool>[] Funcs)
+        {
+            MainLoop(Options, Funcs, new ConsoleColor[] { ConsoleColor.Blue, ConsoleColor.White });
+        }
+
+        #endregion
+
+
+
+        #region Private_Methods
+
+        void MainLoop(string[] Options, Func<bool>[] Option_Funcs, ConsoleColor[] CMD_Colors)
+        {
+            ConsoleKey key;
+            int counter = 0;
+            Console.CursorVisible = false;
+
+            Rendering(counter, Options, CMD_Colors);
+            while (true)
+            {
+                key = Console.ReadKey().Key;
+                if (key == ConsoleKey.UpArrow)
+                {
+                    Rendering(counter = counter > 0 ? counter - 1 : 0, Options, CMD_Colors);
+                }
+                else if (key == ConsoleKey.DownArrow)
+                {
+                    counter = counter < Options.Length - 2 ? counter + 1 : Options.Length - 1;
+                    Rendering(counter, Options, CMD_Colors);
+                }
+                else if (key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    Option_Funcs[counter]();
+                    Task.Delay(500).Wait();
+                    Rendering(counter, Options, CMD_Colors);
+                }
+            }
+
+        }
+
+
+        void Rendering(int counter_render, string[] Options_Render, ConsoleColor[] CMD_Colors)
+        {
+            Console.Clear();
+            for (int i = 0; i < Options_Render.Length; i++)
+            {
+                if (i == counter_render)
+                {
+                    Console.ForegroundColor = CMD_Colors[0];
+                    Console.WriteLine($"-> {Options_Render[i]}");
+                }
+                else
+                {
+                    Console.ForegroundColor = CMD_Colors[1];
+                    Console.WriteLine($"* {Options_Render[i]}");
+                }
+            }
+
+        }
+
+        #endregion
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
+            DC_Menu dot_Selector = new();
 
-            #region User
-
+            //User_Testing
             Func<bool> func = () =>
             {
                 Console.WriteLine("Washoiii");
@@ -20,71 +20,11 @@
             Func<bool>[] funcs = new Func<bool>[2];
             funcs[0] = func;
             funcs[1] = funco;
-            
-            MainLoop(new string[] { "Yokoso", "Kira kira","Doki doki", "Mochi mochi" },
-                                   funcs);
-
-            #endregion
-
-
-
-            #region Selector
-
-            void MainLoop(string[] Options, Func<bool>[] hola)
-            {
-                ConsoleKey key;
-                int counter = 0;
-                Console.CursorVisible = false;
-
-                Rendering(counter, Options);
-                while (true)
-                {
-                    key = Console.ReadKey().Key;
-                    if (key == ConsoleKey.UpArrow)
-                    {
-                        Rendering(counter = counter > 0 ? counter - 1 : 0, Options);
-                    }
-                    else if (key == ConsoleKey.DownArrow)
-                    {
-                        counter = counter < Options.Length - 2 ? counter + 1 : Options.Length - 1;
-                        Rendering(counter, Options);
-                    }
-                    else if (key == ConsoleKey.Enter)
-                    {
-                        Console.Clear();
-                        hola[counter]();
-                        Task.Delay(500).Wait();
-                        Rendering(counter, Options);
-                    }
-                }
-
-            }
-
-            void Rendering(int counter_render, string[] Options_Render)
-            {
-                Console.Clear();
-                for (int i = 0; i < Options_Render.Length; i++)
-                {
-                    if (i == counter_render)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine($"-> {Options_Render[i]}");
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.White;
-                        Console.WriteLine($"* {Options_Render[i]}");
-                    }
-                }
-
-            }
-
-
-            #endregion
-
+            ConsoleColor[] colors = new ConsoleColor[2] {ConsoleColor.White, ConsoleColor.DarkMagenta };
+            dot_Selector.Dot_Menu(new string[] { "Yokoso", "Kira kira","Doki doki", "Mochi mochi" },
+                                   funcs, colors);
         }
 
-        
 
 
 
